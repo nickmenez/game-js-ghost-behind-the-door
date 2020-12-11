9d7768cae8e41ba63a36a2a8ed6fae0a9d036343
Nicolas Menezes <nickolasmenez@gmail.com>
2020-12-11 13:45:17 +1000

    Update get_top10.php

--- get_top10.php
+++ get_top10.php
@@ -1,13 +1,8 @@
 <?php
 
+$conn = mysqli_connect("localhost", "username", "password", "database");
 
-
-$conn = mysqli_connect("localhost", "adminnick", "Gs%+^4m8Xiz&", "playground");
-
-$result = mysqli_query($conn, "SELECT * FROM trapdoor_leaderboard ORDER BY highscore_column DESC LIMIT 10");
-//$result = mysqli_query($conn, "SELECT t.* FROM trapdoor_leaderboard t JOIN ( SELECT username_column, MAX(highscore_column) maxVal FROM trapdoor_leaderboard GROUP BY username_column ORDER BY maxVal DESC LIMIT 10) t2 ON t.highscore_column = t2.maxVal AND t.username_column = t2.username_column;");
-
-
+$result = mysqli_query($conn, "SELECT * FROM table_name ORDER BY highscore_column DESC LIMIT 10");
 
 $data = array();
 while ($row = mysqli_fetch_object($result))
