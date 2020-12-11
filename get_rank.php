6a483840f7d53055190ee37995de536fdaab89e4
Nicolas Menezes <nickolasmenez@gmail.com>
2020-12-11 13:44:20 +1000

    Update get_rank.php

--- get_rank.php
+++ get_rank.php
@@ -3,9 +3,9 @@
 
 $q = $_GET['q'];
 
-$conn = mysqli_connect("localhost", "adminnick", "Gs%+^4m8Xiz&", "playground");
+$conn = mysqli_connect("localhost", "username", "password", "database");
 
-$result = mysqli_query($conn, "SELECT COUNT(*)+1 FROM trapdoor_leaderboard WHERE highscore_column > '$q'");
+$result = mysqli_query($conn, "SELECT COUNT(*)+1 FROM table_name WHERE highscore_column > '$q'");
 
 $data = mysqli_fetch_row($result);
         
